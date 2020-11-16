bf67eb9d9ec811e5500b5700d1d466c890aa0c0b
Martin Ficzel <ficzel@sitegeist.de>
2020-11-16 22:36:45 +0100

    TASK: Neos 6 / Flow 7 Compatibility
    The redirect http component was replaced by a middleware

---
+++ Classes/RedirectMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Neos\RedirectHandler;
+
+/*
+ * This file is part of the Neos.RedirectHandler package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\ServerRequestAttributes;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RedirectMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var RedirectService
+     * @Flow\Inject
+     */
+    protected $redirectService;
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
+    {
+        $routingMatchResults = $request->getAttribute(ServerRequestAttributes::ROUTING_RESULTS);
+        if ($routingMatchResults !== null) {
+            return $next->handle($request);
+        }
+        $response = $this->redirectService->buildResponseIfApplicable($request);
+        if ($response !== null) {
+            return $response;
+        } else {
+            return $next->handle($request);
+        }
+    }
+}
